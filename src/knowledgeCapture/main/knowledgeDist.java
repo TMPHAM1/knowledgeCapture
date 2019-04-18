b10ecbbe165fa98d79ec7b5165a6e0f081c0e08d
Tien Pham <tmpham1@uci.edu>
2019-04-18 10:50:37 -0700

    connected read with write

--- src/knowledgeCapture/main/knowledgeDist.java
+++ src/knowledgeCapture/main/knowledgeDist.java
@@ -27,8 +27,8 @@ public class knowledgeDist {
 //		test.add(test2);
 		ArrayList<String> parsedBlock = new ArrayList<String>();
 		parsedBlock.add("Author: Trent Sanford; LastUpdated: 11/21/90; Description: This function is used for reading and looking for descrtips of other functions;");
-		read(parsedBlock);
-		write(test);
+		List<knowledge> knowledgeList = read(parsedBlock);
+		write(knowledgeList);
 	}
 	
 	public static void write(List<knowledge> knowledgeList) {
@@ -49,37 +49,31 @@ public class knowledgeDist {
 		}
 		
 	}
-	 public static knowledge[] read(ArrayList<String> parsedBlock) {
-		 	knowledge[] knowledgeItemList = null;
+	 public static List<knowledge> read(ArrayList<String> parsedBlock) {
+		 		List<knowledge> knowledgeItemList = new ArrayList<knowledge>();
 		 	for (String knowledgeBlock: parsedBlock) { // For each Block we get 
-		 		//knowledge knowledgeItem = new knowledge();
-		 		
+		 		knowledge newKnowledge = new knowledge();
 		 		 String[] propertyString = knowledgeBlock.split(";"); // Split the String at ; to break each line
 		 		for (String property : propertyString) {
 		 			String[] propertyValue = property.split(":"); //Split the string at the colon to get the property and value;
 		 			switch(propertyValue[0].trim()) { // Set current lines property and value
 		 			case "Author": 
-		 				knowledge.setAuthor(propertyValue[1]);
+		 				newKnowledge.setAuthor(propertyValue[1]);
 		 				break;
 		 			case "LastUpdated":
-		 				knowledge.setLastUpdated(propertyValue[1]);
+		 				newKnowledge.setLastUpdated(propertyValue[1]);
 		 				break;
 		 			case "Description":	
-		 				knowledge.setDescription(propertyValue[1]);
+		 				newKnowledge.setDescription(propertyValue[1]);
 		 				break;
 		 			default:
 		 				break;
 		 			}
 		 				
 		 		}
-		 		
+		 		knowledgeItemList.add(newKnowledge);
 		 		
 		 	}
-		 	
-		 	
-		 	
-		 	
-		 	
 			return knowledgeItemList;
 	 }
 
