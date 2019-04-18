7bb7fbd494d0390520f470f3c7d05001dfbef5a9
Trenton Sanford <trentonsanford@Trentons-MacBook-Pro.local>
2019-04-18 11:33:18 -0400

    model is made

--- src/knowledgeCapture/model/knowledge.java
+++ src/knowledgeCapture/model/knowledge.java
@@ -1,5 +1,28 @@
 package knowledgeCapture.model;
 
-public class knowledge {
+import java.util.Date;
 
+public class knowledge {
+	private String author;
+	private Date lastUpdated;
+	private String description;
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
